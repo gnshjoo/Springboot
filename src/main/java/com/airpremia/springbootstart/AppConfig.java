a5aa76316f37575299b303debcf6e37407008a9f
성훈주 <gnshjoo@gmail.com>
2024-01-24 10:20:54 +0900

    spring 추가

--- src/main/java/com/airpremia/springbootstart/AppConfig.java
+++ src/main/java/com/airpremia/springbootstart/AppConfig.java
@@ -2,7 +2,7 @@ package com.airpremia.springbootstart;
 
 
 import com.airpremia.springbootstart.Discount.DiscountPolicy;
-import com.airpremia.springbootstart.Discount.FixDiscountPolicy;
+
 import com.airpremia.springbootstart.Discount.RateDiscountPolicy;
 import com.airpremia.springbootstart.Member.MemberRepository;
 import com.airpremia.springbootstart.Member.MemberService;
@@ -10,23 +10,28 @@ import com.airpremia.springbootstart.Member.MemberServiceImpl;
 import com.airpremia.springbootstart.Member.MemoryMemberRepository;
 import com.airpremia.springbootstart.Order.OrderService;
 import com.airpremia.springbootstart.Order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 실제 동작에 필요한 구현 객체 생성
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy(); // 이것만 수정하면 DiscountPolicy 를 변경할 수 있음
+ // return new FixDiscountPolicy(); // 이것만 수정하면 DiscountPolicy 를 변경할 수 있음
         return new RateDiscountPolicy();
     }
 
---
+++ src/main/java/com/airpremia/springbootstart/MemberApp.java
@@ -0,0 +1,24 @@
+package com.airpremia.springbootstart;
+
+import com.airpremia.springbootstart.Member.Grade;
+import com.airpremia.springbootstart.Member.Member;
+import com.airpremia.springbootstart.Member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
