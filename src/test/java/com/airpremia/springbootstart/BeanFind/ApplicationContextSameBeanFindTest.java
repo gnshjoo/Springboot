2617e01fae2d1a9e21c431cd4a7857ff2d4e3ed6
성훈주 <gnshjoo@gmail.com>
2024-01-24 10:53:37 +0900

    spring Bean 같은 이름 빈 조회

---
+++ src/test/java/com/airpremia/springbootstart/BeanFind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,58 @@
+package com.airpremia.springbootstart.BeanFind;
+
+import com.airpremia.springbootstart.AppConfig;
+import com.airpremia.springbootstart.Discount.DiscountPolicy;
+import com.airpremia.springbootstart.Member.MemberRepository;
+import com.airpremia.springbootstart.Member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByTypeDuplicate() {
+        assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(MemberRepository.class));
+
+    }
+
+
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByName() {
+        MemberRepository memberRepository1 = ac.getBean("memberRepository1", MemberRepository.class);
+        assertThat(memberRepository1).isInstanceOf(MemberRepository.class);
+    }
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        MemberRepository memberRepository1 = ac.getBean("memberRepository1", MemberRepository.class);
+        MemberRepository memberRepository2 = ac.getBean("memberRepository2", MemberRepository.class);
+        assertThat(memberRepository1).isInstanceOf(MemberRepository.class);
+        assertThat(memberRepository2).isInstanceOf(MemberRepository.class);
+    }
+
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
