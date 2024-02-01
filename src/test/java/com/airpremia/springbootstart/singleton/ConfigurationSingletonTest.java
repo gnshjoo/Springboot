96af02026adbf5ba43cf5d9b432bc13ea909d254
성훈주 <gnshjoo@gmail.com>
2024-02-01 14:52:07 +0900

    configuration 정리

---
+++ src/test/java/com/airpremia/springbootstart/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,37 @@
+package com.airpremia.springbootstart.singleton;
+
+import com.airpremia.springbootstart.AppConfig;
+import com.airpremia.springbootstart.Member.MemberRepository;
+import com.airpremia.springbootstart.Member.MemberServiceImpl;
+import com.airpremia.springbootstart.Order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        assertThat(memberRepository1).isSameAs(memberRepository2);
+
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass()); //클래스 타입이 뭔지 확인 가능
+    }
+}
